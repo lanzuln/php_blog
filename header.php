f2112066dd25ae5377f3718dbb11a9db276d54ac
lanzuln <lanzu.dev@gmail.com>
2025-01-18 20:35:26 +0600

    update

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hikers Blog &mdash; Colorlib Website Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="assets/css/aos.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+    <header class="site-navbar pt-3" role="banner">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+
+          <div class="col-6 col-xl-6 logo">
+            <h1 class="mb-0"><a href="index.html" class="text-black h2 mb-0">Hikers</a></h1>
+          </div>
+          
+          <div class="col-6 mr-auto py-3 text-right" style="position: relative; top: 3px;">
+            <div class="social-icons d-inline">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-instagram"></span></a>
+            </div>
+            <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-xl-none"><span class="icon-menu h3"></span></a></div>
+          </div>
+          
+          <div class="col-12 d-none d-xl-block border-top">
+            <nav class="site-navigation text-center " role="navigation">
+
+              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block mb-0">
+                <li class="active"><a href="index.html">Homepage</a></li>
+                <li><a href="category.html">Lifestyle</a></li>
+                <li class="has-children">
+                  <a href="category.html">Inspiration</a>
+                  <ul class="dropdown">
+                    <li><a href="category.html">Architect</a></li>
+                    <li><a href="category.html">Minimal</a></li>
+                    <li><a href="category.html">Interior</a></li>
+                    <li><a href="category.html">Furniture</a></li>
+                  </ul>
+                </li>
+                <li><a href="category.html">Technology</a></li>
+                <li><a href="category.html">Latest</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+      </div>
+    </header>
